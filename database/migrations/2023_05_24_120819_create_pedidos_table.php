0461914d94b0aa58df0589d91fab0522615560dc
Gabriel Luiz <gabrielluizcm@hotmail.com>
2023-05-24 09:20:09 -0300

    Migrations das tabelas de clientes e pedidos

---
+++ database/migrations/2023_05_24_120819_create_pedidos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_cliente');
+            $table->foreign('id_cliente')->references('id')->on('clientes');
+            $table->date('data_entrega');
+            $table->float('valor_pedido', 15, 2);
+            $table->float('valor_frete', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
